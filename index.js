2a28698911aa24c6eba8394ace4a20671239b9d5
ozca24tl <ozca24tl@gmail.com>
2026-06-25 14:32:52 +0200

    string

---
+++ index.js
@@ -0,0 +1,29 @@
+let a = 1; 
+let b = 1;
+let c = 1;
+let d = 1;
+
+a -= 1;
+b += 1;
+c *= 1;
+d /= 1;
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+//console.log(d);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+let animal = "dog";
+
+animal[0]; //--------> first letter of the string 
+console.log(animal[1]); //--------> second letter of the string 
+
+console.log(
+animal.toUpperCase()
+);
+
+let madDog = animal.toUpperCase();
+
+console.log(
+madDog.toUpperCase()
+);
