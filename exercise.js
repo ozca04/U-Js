0b1efd8895a1babc3e61e85ea22cb0fea45512b2
ozca24tl <ozca24tl@gmail.com>
2026-06-27 23:16:37 +0200

    s

--- exercise.js
+++ exercise.js
@@ -7,3 +7,7 @@ die2: ${die2}
 roll: "You rolled a ${die1} and a ${die2}. They sum to ${die1 + die2}"`
 
 console.log (roll);
+
+prompt("please enter")
+
+//add the js file in the end of body, inside of it 
---
+++ notes.js
@@ -0,0 +1,6 @@
+let random = Math.floor(Math.random() * 10);
+if (random >= 5){
+    console.log(`Your number '${random}' is bigger or equal to 5`)
+}else{
+    console.log(`Your number '${random}' is smaller than 5`)
+}
