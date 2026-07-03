679d4eab438a5ff29447d0819d5db383f06f5d70
ozca24tl <ozca24tl@gmail.com>
2026-07-03 17:19:38 +0200

    initial commit

---
+++ Arrays/creatingArrays.js
@@ -0,0 +1,6 @@
+//array of strings
+let carBrands = ['Audi', 'Nissan', 'Honda'];
+//array of numbers
+let modelYear = [1990, 2000, 2010, 2020];
+
+console.log(modelYear);
---
+++ calculator.js
@@ -0,0 +1,19 @@
+let a = Number(prompt("enter the first number"));
+let b = Number(prompt("enter the second number"));
+let calculation = prompt("enter whatu want to do");
+
+if(typeof a === "number" && typeof b === "number"){
+    if (calculation === "*" || calculation === "x"){
+        document.getElementById("output").textContent = a * b;
+    }else if(calculation === "/"){
+        document.getElementById("output").textContent = a / b;
+    }else if(calculation === "+"){
+        document.getElementById("output").textContent = a + b;
+    }else if(calculation === "-"){
+        document.getElementById("output").textContent = a - b;
+    }else{
+        document.getElementById("output").textContent = "undefined calcuation";
+    }
+}else{
+    document.getElementById("output").textContent = "this is not a number";
+}
