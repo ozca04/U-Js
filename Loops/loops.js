1ff880f866c78ce34ecadd8728bba485934afab1
ozca24tl <ozca24tl@gmail.com>
2026-07-06 00:04:34 +0200

    loops-halfway

---
+++ Loops/loops.js
@@ -0,0 +1,28 @@
+//for --->
+//for (let i = 1; i <= 10; i++){
+//    console.log(i);
+//    console.log('mamut')
+//}
+//for( let i = 0; i <= 20; i += 2){
+//    console.log(i)
+//}
+
+const animals = ['lions', 'tigers', 'bears'];
+
+for (let i = 0; i < animals.length; i++){
+    console.log(i, animals[i])
+}
+
+let carBrands = ['audi', 'nissan', 'hyundai', 'volvo', 'vw', 'bmw'];
+
+for(let i = 0; i < carBrands.length; i++){
+    console.log(carBrands[3])
+}
+
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+
+for(let i = 0; i < people.length; i++){
+    console.log(people[i].toUpperCase());
+}
