a122fcc43348982f1074b8862a29ed2a145f4be7
ozca24tl <ozca24tl@gmail.com>
2026-07-05 20:33:02 +0200

    arrays done

---
+++ Arrays/nesting-arrays.js
@@ -0,0 +1,13 @@
+//nest arrays in objects or nest objects in array 
+const cars = [
+    {brand: 'audi', year: 2005, milage:20030 },
+    {brand: 'nissan', year:2002, milage:32000},
+    {beand: 'bmw', year: 2012, milage:20000}
+]//objects nested in array 
+
+const studentList = {
+    className: '12-B',
+    classFloor: 3,
+    exams:['Math', 'Chem', 'Psy'],
+    students:['ahmet', 'mehmet', 'ayse', 'fatma']
+}//arrays nested in an object
---
+++ Objects/modifyingObjects.js
@@ -0,0 +1,4 @@
+const points = {ahmet: 30, mehmet: 50}
+
+points.ahmet = 43;
+points['mehmet'] = 59;
