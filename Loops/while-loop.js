d3a907e8c78b8d3b8ee84b23f8a2d6d1a65032c6
ozca24tl <ozca24tl@gmail.com>
2026-07-06 16:52:54 +0200

    guessing game

--- Loops/while-loop.js
+++ Loops/while-loop.js
@@ -1,5 +1,22 @@
-let num = 0;
-while (num < 10){
-    console.log(num);
-    num++;
-}
+// let num = 0;
+// while (num < 10){
+//     console.log(num);
+//     num++;
+// }
+
+const SECRET = "osman";
+
+let guess = prompt("enter the secret code...")
+// while(guess !== SECRET){
+//     guess = prompt("enter the secret code...")
+// }
+// console.log("correct")
+
+//-------BREAK
+let input = prompt("say something")
+while (true){
+    input = prompt(input);
+    if(input === "stop"){
+        break;
+    }
+}console.log("u won")
---
+++ exercises/guessing-game.js
@@ -0,0 +1,3 @@
+let number = parseInt(prompt("enter a number "))
+
+Math.floor(Math.random() * number) + 1
