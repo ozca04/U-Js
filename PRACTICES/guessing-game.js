3b234f0640ea78fec75dcd12d85306c47182bcfd
ozca24tl <ozca24tl@gmail.com>
2026-07-06 23:11:29 +0200

    for---in loops done

--- PRACTICES/guessing-game.js
+++ PRACTICES/guessing-game.js
@@ -3,6 +3,23 @@ while(!number){
     let number = parseInt(prompt("enter a VALID number "))
 }
 
-const targetNum = Math.floor(Math.random() * number) + 1
 
-console.log(targetNum)
+const targetNum = Math.floor(Math.random() * number) + 1
+let attempts = 1;
+
+let guess = parseInt(prompt("enter your first guess"));
+while(parseInt(guess) !== targetNum){
+    if(guess === "q") break;
+    attempts++;
+    if(guess> targetNum){
+        guess = prompt("too high");
+    }else{
+        guess = prompt("too low")
+    }
+}
+
+if(guess === "q"){
+    console.log("u failed")
+}
+
+console.log(` you got it. it took ${attempts} attmepts`)
