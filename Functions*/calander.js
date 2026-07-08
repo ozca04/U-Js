9605e2a31d344782f0053ec526fca4ff56921d83
ozca24tl <ozca24tl@gmail.com>
2026-07-08 23:15:20 +0200

    function practice

--- Functions*/calander.js
+++ Functions*/calander.js
@@ -1,20 +1,30 @@
+// function returnDay(day){
+//     if(day === 1){
+//         return 'monday'
+//     }else if(day === 2){
+//         return 'tuesday'
+//     }else if(day === 3){
+//         return 'Wednesday'
+//     }else if(day === 4){
+//         return 'Thursday'
+//     }else if(day === 5){
+//         return 'Friday'
+//     }else if(day === 6){
+//         return 'Saturday'
+//     }else if(day === 7){
+//         return 'Sunday'
+//     }else{
+//         return null
+//     }
+// }
+// returnDay(5)
+
+///////SHORTER WAY////////
+
 function returnDay(day){
-    if(day === 1){
-        return 'monday'
-    }else if(day === 2){
-        return 'tuesday'
-    }else if(day === 3){
-        return 'Wednesday'
-    }else if(day === 4){
-        return 'Thursday'
-    }else if(day === 5){
-        return 'Friday'
-    }else if(day === 6){
-        return 'Saturday'
-    }else if(day === 7){
-        return 'Sunday'
-    }else{
+    if (day < 1 || day >7){
         return null
     }
+    const days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+    return days[day-1];
 }
-returnDay(5)
