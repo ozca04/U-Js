e516fa9ba3cd68e8da9d4ed53c45d8deea2acbb4
ozca24tl <ozca24tl@gmail.com>
2026-07-09 20:50:20 +0200

    serenin anasi

---
+++ Functions*/word-counter.js
@@ -0,0 +1,24 @@
+// function countBigWords (word){
+//      let arr = 0;
+//      for(let i = 0; i < word.length; i++){
+//         if(word[i].length > 3){
+//             arr++
+//         }
+//      }
+//      return arr
+//  }
+//  console.log(countBigWords(["hi", "hello", "cat", "world"]))
+//  ///////////////////////////
+
+
+function onlyEvens(numm){
+    let evens = [];
+    for(let i = 0; i < numm.length; i++){
+        if(numm[i] % 2 === 0){
+            evens.push(numm[i])
+        }
+    }
+    return evens
+}
+    console.log(onlyEvens([1, 2, 3, 4, 5, 6, 8, 7, 10]))
+
---
+++ advanced-functions/returning-Function.js
@@ -0,0 +1,51 @@
+// function gamble(){
+//     const rand = Math.random()
+//     if (rand > 0.5){
+//         return function(){
+//             console.log('jackpot')
+//         }
+//     }else{
+//         return function(){
+//             console.log('lose')
+//         }
+//     }
+// }
+// const spin = gamble()
+// spin()
+
+//////////////////////////////////////////////////////////////////////////////
+
+// function age(min, max){
+//     return function (num){
+//         return num >= min && num <=max;
+//     }
+// }
+// const adult = age(19, 65)
+// const child = age(0, 18)
+// console.log(child(35))
+
+//////////////////////////////////////////////////////////////////////////////
+// const math = {
+//     multiply : function(x, y){
+//         return x * y
+//     },
+//     divide : function(x, y){
+//         return x / y
+//     },
+//     square : function(x){
+//         return x * x
+//     }
+// }
+
+// console.log(math.divide(10, 5))
+////////////////////////////////////////////////////////////////////////////////
+
+const square = {
+    area : function(side){
+        return side * side;
+    },
+    perimeter : function(side){
+        return side * 4;
+    }
+}
+console.log(square.perimeter(17))
