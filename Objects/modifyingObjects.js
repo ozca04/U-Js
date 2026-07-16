e0a65678ea212e9064fa922daa0e85fa914c714a
ozca24tl <ozca24tl@gmail.com>
2026-07-16 13:29:23 +0200

    replace old notes with organized structure

--- Objects/modifyingObjects.js
+++
@@ -1,4 +0,0 @@
-const points = {ahmet: 30, mehmet: 50}
-
-points.ahmet = 43;
-points['mehmet'] = 59;
--- advanced-functions/try/catch.js
+++
@@ -1,5 +0,0 @@
-try{
-    hellot.toUpperCase()//this is an error
-} catch{
-    console.log('error')
-}
