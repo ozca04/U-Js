b56671c3d2063a59630533edd818d6603f418019
ozca24tl <ozca24tl@gmail.com>
2026-07-24 17:22:05 +0200

    ss

--- 12-DOM-events/3-formEvents/formevents.js
+++ 12-DOM-events/3-formEvents/formevents.js
@@ -1,5 +1,11 @@
 const form = document.querySelector('#shelterForm');
+const input = document.querySelector('#catName')
+const list = document.querySelector('#cats')
 form.addEventListener('submit', function(e){
     e.preventDefault();
-    console.log('SUBMITTED');
+    const catName = input.value;
+    const newLi = document.createElement('Li');
+    newLi.innerText = catName;
+    list.append(newLi)
+    input.value = "";
 });
