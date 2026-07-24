a35d7ddd692c620587aaa895232ccfe62dd5e5ac
ozca24tl <ozca24tl@gmail.com>
2026-07-24 17:25:28 +0200

    ss

---
+++ exercise /app.js
@@ -0,0 +1,8 @@
+// Leave the next line, the form must be assigned to a variable named 'form' in order for the exercise test to pass
+const form = document.querySelector('form');
+const product = document.querySelector('#product');
+const qty = document.querySelector('#qty');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+});
