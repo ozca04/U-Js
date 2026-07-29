bd0ba74e25c0ac089ec5759536d2d37b136be338
ozca24tl <ozca24tl@gmail.com>
2026-07-30 01:14:31 +0200

    push

--- 0-practices/asyncPractice.js
+++ 0-practices/asyncPractice.js
@@ -79,8 +79,3 @@
 //   }
 // }
 // result();
-
-function double(n) {
-  n * 2;
-}
-console.log(double(5));
--- generalPractice.js
+++ generalPractice.js
@@ -2,3 +2,44 @@
 //   return n * 2;
 // }
 // console.log(double(5));
+
+// function isEven(n) {
+//   return n % 2 === 0;
+// }
+// console.log(isEven(10));
+
+// function sumArray(nums) {
+//   let result = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     result = result + nums[i];
+//   }
+//   return result;
+// }
+// console.log(sumArray([1, 2, 3, 4]));
+
+// function findFirstBig(nums) {
+//   let x = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] > 100) {
+//       return nums[i];
+//     }
+//   }
+//   return null;
+// }
+// console.log(findFirstBig([5, 50, 3]));
+
+// function countBig(nums) {
+//   let bigNums = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] > 100) {
+//       bigNums++;
+//     }
+//   }
+//   return bigNums;
+// }
+// console.log(countBig([5, 200, 3, 400, 150]));
+
+// function onlyBig(nums) {
+//   return nums.filter((n) => n > 100);
+// }
+// console.log(onlyBig([5, 200, 3, 400]));
