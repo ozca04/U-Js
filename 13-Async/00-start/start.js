ab70f8f72477058c7c0b154776e4c5e23ac62b12
ozca24tl <ozca24tl@gmail.com>
2026-07-29 02:08:17 +0200

    push

---
+++ 13-Async/00-start/start.js
@@ -0,0 +1,52 @@
+//CALL STACK -->latenflip.com or console breakpoint, helps u keep track of what is going on and debug
+
+// //
+// console.log(1);
+// setTimeout(() => console.log(2), 1000);
+// console.log(3);
+// setTimeout(() => console.log(4), 0);
+// console.log(5);
+
+// setTimeout(() => {
+//   document.body.style.backgroundColor = "red";
+//   setTimeout(() => {
+//     document.body.style.backgroundColor = "blue";
+//     setTimeout(() => {
+//       document.body.style.backgroundColor = "orange";
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+///CALLBACK HELL
+const delayedColorChange = (newColor, delay, doNext) => {
+  setTimeout(() => {
+    document.body.style.backgroundColor = newColor;
+    doNext();
+  }, delay);
+};
+
+delayedColorChange("red", 1000, () => {
+  delayedColorChange("orange", 1000, () => {
+    delayedColorChange("yellow", 1000, () => {
+      delayedColorChange("green", 1000, () => {
+        delayedColorChange("blue", 1000, () => {});
+      });
+    });
+  });
+});
+
+// delayedColorChange("purple", 1000);
+// delayedColorChange("pink", 2000);
+// delayedColorChange("olive", 3000);
+
+searchMoveiesAPI("amadeus", () => {
+  saveToMyDB(
+    movies,
+    () => {
+      //if it works run this
+    },
+    () => {
+      //if it doesnt work run this
+    },
+  );
+});
